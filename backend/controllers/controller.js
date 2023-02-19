d4b5e5ab43eccf2a14ecb6aab8ec47706dd94d33
dom <davide.domini2@unibo.it>
2023-02-19 16:30:55 +0100

    feat(backend): add simple signup controller

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -3,7 +3,13 @@ exports.recentTheories = (req, res) => {
 }
 
 exports.signup = (req, res) => {
-
+    res.header('Access-Control-Allow-Origin', '*');
+    let user = req.body.user;
+    console.log(`Name: ${user.name}`);
+    console.log(`Surname: ${user.surname}`);
+    console.log(`Email: ${user.email}`);
+    console.log(`Username: ${user.username}`);
+    console.log(`Password: ${user.password}`);
 }
 
 exports.signin = (req, res) => {
