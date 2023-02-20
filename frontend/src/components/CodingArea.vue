5dd1f24c8a1e76359b06e41b802bc0a1661d9af4
dom <davide.domini2@unibo.it>
2023-02-20 10:37:36 +0100

    feat(frontend): update code area css

--- frontend/src/components/CodingArea.vue
+++ frontend/src/components/CodingArea.vue
@@ -14,12 +14,14 @@ export default {
     methods: {
         solve() {
             console.log(this.code)
+            this.result += "\n Solve Pressed"
         },
         solveAll() {
             console.log(this.query)
+            this.result += "\n SolveAll Pressed"
         },
         reset() {
-            
+            this.result = "Query result:"
         }
     }
 }
@@ -28,22 +30,17 @@ export default {
 
 
 <template>
-    
     <div class='code-container'>
         <VueCodemirror v-model="code" />
-        <textarea class="result" readonly> {{ result }} </textarea>
-        <textarea class="query-area" v-model="query" placeholder="Insert the query here..." cols="91" />
+        <textarea class="result" rows="7" readonly> {{ result }} </textarea>
+        <textarea class="query-area" rows="3" v-model="query" placeholder="Insert the query here..." />
     </div>
     
-    
-    
-    <div>
+    <div class="buttons-container">
         <Button label="Solve" class="p-button-custom" @click="solve()" />
         <Button label="Solve All"  class="p-button-custom" @click="solveAll()" /> 
         <Button label="Reset" class="p-button-custom" @click="reset()" /> 
     </div>
-
-    
 </template>
 
 
