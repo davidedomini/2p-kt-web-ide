55c070643cc58e3a13d2a9b876d2a90331882262
dom <davide.domini2@unibo.it>
2023-06-12 15:12:39 +0200

    feat(backend): implement mqtt service

---
+++ backend/services/MQservice.js
@@ -0,0 +1,27 @@
+var amqp = require('amqplib/callback_api');
+
+let ch = null
+const CONN_URL = 'amqp://guest:guest@localhost:5672/'
+
+exports.connectToChannel = () => {
+    amqp.connect(CONN_URL, function (err, conn) {
+        console.log("[Backend] Connected to RabbitMQ")
+        conn.createChannel(function (err, channel) {
+            console.log("[Backend] Channel created")
+            ch = channel
+            initResponsesQueue()
+        })
+    })
+}
+
+exports.publishToQueue = (queueName, data) => {
+    console.log("[Backend] Sending message")
+    ch.sendToQueue(queueName, new Buffer.from(data))
+} 
+
+function initResponsesQueue() {
+    ch.consume('responses', function (msg) {
+        console.log("[Backend] Received message:" + msg.content.toString())
+        //TODO send response to frontendx
+    }, {noAck: true})
+}
